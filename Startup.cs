e4be42f7a7d0be8397f3d2ed0bb0b627b3b0d324
cangjie <cangjie@hotmail.com>
2023-02-19 13:09:37 +0800

    show swagger

--- Startup.cs
+++ Startup.cs
@@ -35,14 +35,14 @@ namespace LuqinMiniAppBase
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LuqinMiniApp v1"));
                 
-            }
+            //}
 
             app.UseRouting();
             app.UseStaticFiles();
