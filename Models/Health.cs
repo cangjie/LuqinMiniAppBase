e96a16aab43cdcba97a56efa8852ee7ef9ba4fac
cangjie <cangjie@hotmail.com>
2023-05-31 20:53:16 +0800

    mod fields

--- Models/Health.cs
+++ Models/Health.cs
@@ -9,15 +9,22 @@ namespace LuqinMiniAppBase.Models
 	{
 		public int id { get; set; }
 		public string name { get; set; }
-		public string gender { get; set; }
+		
 		public string disease { get; set; }
 		public string depression_level { get; set; }
-		public int use_drug { get; set; }
+		public string other_diseases { get; set; }
+
+        public int use_drug { get; set; }
 		public string drugs_memo { get; set; }
-		public int special_diet { get; set; }
-		public string diet_memo { get; set; }
+
+		public int is_allergy { get; set; }
+		public string allergy { get; set; }
+
 		public int need_others_service { get; set; }
 		public string service_memo { get; set; }
+
+		public int have_sport_disease { get; set; }
+		public string sport_disease_memo { get; set; }
 
     }
 }
